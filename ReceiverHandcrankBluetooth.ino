c2d7d205ec307a1c6dd2942df6e9de09e8511c02
Kenneth Chee <42662921+knnthhtnnk@users.noreply.github.com>
2023-07-23 19:25:12 +0800

    Add files via upload

---
+++ ReceiverHandcrankBluetooth.ino
@@ -0,0 +1,182 @@
+//Library for Bluetooth
+#include <SoftwareSerial.h>
+
+#include "IRLibAll.h"
+
+//Library for Neopixel
+#include <Adafruit_NeoPixel.h>
+
+//Vulnerable Mode and Invincible Mode Pin
+#define PSEUDO_5V 3
+#define NPX_PIN  15
+//Neopixel
+#define NUMPIXELS 10
+
+//TIME INTERVAL
+#define STACK_CHECKUP_INTERVAL 1000
+
+const byte address[6] = "00001";
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, NPX_PIN, NEO_GRB + NEO_KHZ800);
+
+int slowStack = 0;
+int iteration_bluetooth;
+unsigned long lastSignal, lastStackCheck, lastIRReading, invincibleModeStart;
+bool switch_, emitterFlag=0, invincibleMode=0;
+char buffer_[32];
+unsigned char text;
+float averageLDRReading, iteration;
+int ldrReading;
+
+SoftwareSerial mySerial(10, 11);
+
+//Setup IR
+IRrecvPCI myReceiver(2);
+IRdecode myDecoder;
+
+//Setup Motor
+int EN1 = 6;
+int EN2 = 5;  //Roboduino Motor shield uses Pin 9
+int IN1 = 7;
+int IN2 = 4; //Latest version use pin 4 instead of pin 8
+
+
+void Motor1(int pwm, boolean reverse) {
+  analogWrite(EN1, pwm); //set pwm control, 0 for stop, and 255 for maximum speed
+  if (reverse)  {digitalWrite(IN1, HIGH);}
+  else  {digitalWrite(IN1, LOW);}
+}
+
+void Motor2(int pwm, boolean reverse) {
+  analogWrite(EN2, pwm);
+  if (reverse)  {digitalWrite(IN2, HIGH);}
+  else  {digitalWrite(IN2, LOW);}
+}
+
+void setup() {
+  //Serial begin
+  Serial.begin(9600);
+  //Setup RF
+  mySerial.begin(57600);
+  //Setup neopixel
+  pixels.begin();
+
+  for (int i = 4; i <= 7; i++) pinMode(i, OUTPUT);//pin for motor
+
+  //Setup Pseudo 5v for IR receiver
+  pinMode(PSEUDO_5V, OUTPUT);
+  digitalWrite(PSEUDO_5V, HIGH);
+  //
+  //  //Start Test Code
+  //  Motor1(255,0);
+  //  Motor2(0, 0);
+  //  delay(500);
+  //  Motor1(0,1);
+  //  Motor2(255, 0);
+  //  delay(500);
+  //  Motor1(0, 1);
+  //  Motor2(255, 0);
+  //  delay(500);
+  //  Motor1(0,1);
+  //  Motor2(255, 1);
+  //  delay(500);
+  //  Motor1(0, 0);
+  //  Motor2(0, 0);
+
+}
+
+void testMotor(){
+   Motor1(255,0);
+   Motor2(0, 0);
+   delay(500);
+   Motor1(0,1);
+   Motor2(255, 0);
+   delay(500);
+   Motor1(0, 1);
+   Motor2(255, 0);
+   delay(500);
+   Motor1(0,1);
+   Motor2(255, 1);
+   delay(500);
+   Motor1(0, 0);
+   Motor2(0, 0);
+}
+
+void loop() {
+  //Clearing buffer to read
+  //  Serial.print("time: ");
+  //  Serial.println(millis());
+
+  //Listening RF
+  if(mySerial.available()>0){
+    delay(20);
+    iteration_bluetooth = 0;
+    while(mySerial.available()>0&&iteration_bluetooth<32){
+      buffer_[iteration_bluetooth] = mySerial.read();
+      iteration_bluetooth++;
+    }
+    Serial.println(buffer_);
+  }
+  //Check whether the first char is '@'
+  if(buffer_[0] != '@') {
+    strcpy(buffer_, "");
+  }
+  if(buffer_[0] == '@') {
+    if(strlen(buffer_) >= 9){
+      //A timeout of
+      if(buffer_[8] == '!') {
+        lastSignal = millis();
+        //RIGHT SIDE
+        if(buffer_[3]>30) {
+          //motor1_value = (int)map(buffer_[3], 0, 127, 0, 255)
+          Motor1(((int)map(buffer_[3], 0, 127, 0, 255)), false);
+          Serial.println(map(buffer_[3], 0, 127, 0, 255));
+        }
+        else if(buffer_[3] < -30) {
+          Motor1(((int)map(buffer_[3], 0, -127, 0, 255)), true);
+          Serial.println(map(buffer_[3], 0, -127, 0, 255));
+        }
+        else{
+          Motor1(0, false);
+        }
+        //LEFT SIDE
+        if(buffer_[4]> 30) {
+          Motor2(((int)map(buffer_[4], 0, 127, 0, 255)), false);
+          Serial.println(map(buffer_[4], 0, 127, 0, 255));
+        }
+        else if(buffer_[4] < -30) {
+          Motor2(((int)map(buffer_[4], 0, -127, 0, 255)), true);
+          Serial.println(map(buffer_[4], 0, -127, 0, 255));
+        }
+        else{
+          Motor2(0, false);
+        }
+        switch_ = 0;
+      }
+    }
+  }
+  //A timeout, if no correct signal is received for one second the motor will stop
+
+  if(millis()>lastIRReading+100){
+    myReceiver.enableIRIn();      //Restart receiver
+    lastIRReading = millis();
+    if (myReceiver.getResults()&&invincibleMode == 0) {
+      Serial.println("pewpew");
+      myDecoder.decode();           //Decode it
+      myDecoder.dumpResults(false);  //Now print results. Use false for less detail
+      myReceiver.enableIRIn();      //Restart receiver
+      Motor1(0, false);
+      Motor2(0, false);
+      delay(1000);
+      invincibleModeStart = millis();
+      invincibleMode = 1;
+    }
+  }
+  //if got hit with IR, will stop for 1 second, after that it will enter invincible mode for 2 seconds, which means cannot be shot by IR for that duration
+  if(millis()>invincibleModeStart + 2000){invincibleMode = 0;}
+  //
+  if(millis () > (lastSignal + 1000)){
+    Motor1(0, false);
+    Motor2(0, false);
+  }
+}
---
+++ SenderHandcrankBluetooth.ino
@@ -0,0 +1,204 @@
+
+//MP3 module library
+#include <SoftwareSerial.h>
+#include "RedMP3.h"
+//Neopixel library
+#include <Adafruit_NeoPixel.h>
+
+#define RIGHT_SIDE_PIN A4
+#define LEFT_SIDE_PIN A5
+
+#define SENDING_INTERVAL 80 //in millisecond
+
+#define SOUND_NPX_PIN 3
+#define RIGHT_NPX_PIN 4
+#define LEFT_NPX_PIN 5
+#define NUMPIXELS 6
+
+#define MP3_RX 7//RX of Serial MP3 module connect to D7 of Arduino
+#define MP3_TX 8//TX to D8, note that D8 can not be used as RX on Mega2560, you should modify this if you donot use Arduino UNO
+MP3 mp3(MP3_RX, MP3_TX);
+
+#define DEBUG 0
+
+double rightForwardPixels;
+double leftForwardPixels;
+SoftwareSerial mySerial(10, 11);
+char text[32];
+String command_;
+
+long currentTime;
+double iteration;
+
+double averageReadingRight;
+double averageReadingLeft;
+double averageReadingGrabber;
+double chargeRight;
+double chargeLeft;
+
+double chargeMeter;
+
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, SOUND_NPX_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel rightChargePixels = Adafruit_NeoPixel(NUMPIXELS, RIGHT_NPX_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leftChargePixels = Adafruit_NeoPixel(NUMPIXELS, LEFT_NPX_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  delay(500);
+  //Serial begin
+  Serial.begin(9600);
+  //RF Setup
+  mySerial.begin(57600);
+
+  command_="@A1XXXXX!";
+  command_.toCharArray(text,32);
+  //Polling setup
+  currentTime=millis();
+  iteration = 1;
+  averageReadingRight=0;
+  averageReadingLeft=0;
+  averageReadingGrabber=0;
+  //MP3 setup
+  mp3.allCycle();
+  delay(50);
+//  mp3.nextSong();
+//Setup CHARGE METER
+  chargeLeft = 500;
+  chargeRight = 500;
+  chargeMeter = 500;
+//Setup neopixel
+  pixels.begin();
+  rightChargePixels.begin();
+  leftChargePixels.begin();
+}
+
+void loop() {
+  if(millis()>currentTime+SENDING_INTERVAL){
+    text[0]='@';
+    text[1]='A';
+    text[2]='1';
+    text[6]='X';//Grabber open and close
+    text[7]='X';//IR Emitter on/off
+    text[8]='!';
+
+// RIGHT WHEEL READING
+//    averageReadingRight = 500;//Guard for if RIGHT WHEEL IS NOT ALR ATTACHED
+    chargeRight = chargeRight + map((int)averageReadingRight, 0, 1023, -1200, 1200);
+    if(chargeRight>550)
+      chargeRight = chargeRight - map(chargeRight, 500, 1023, 80, 350);//So chargheRight tend to go to 500
+    if(chargeRight<450)
+      chargeRight = chargeRight + map(chargeRight, 500, 0, 80, 350);//So chargheRight tend to go to 500
+
+    if(chargeRight>1023)
+      chargeRight = 1023;//GUARD so it is bounded
+    if(chargeRight<0)
+      chargeRight = 0;
+
+    text[3] = map((int)chargeRight, 0, 1023, -127, 127);//THE THING THAT IS SENT
+
+    //GUARD SO THE VALUE DOESN'T COME AS WEIRD
+    if(text[3]==0||text[3]==9||text[3]==10||text[3]==13||text[3]==32){
+      text[3] = 1;
+    }
+    if(text[3]==127){
+      text[3] = 126;
+    }
+//    Serial.println((int)text[3]);
+
+    // Serial.print("Charge Right: ");
+    // Serial.println(chargeRight);
+
+    Serial.print("Average Reading Right: ");
+    Serial.print(averageReadingRight);
+    Serial.print(" ");
+
+    //Right Charge Pixels Indicator
+      //forward
+    if(chargeRight>500){
+      rightForwardPixels = (int)map(chargeRight, 500, 1023, 0, NUMPIXELS);
+      for(int i=0;i<rightForwardPixels;i++){rightChargePixels.setPixelColor(i, rightChargePixels.Color(25,0,0));}//GREEN
+      for(int i=rightForwardPixels;i<NUMPIXELS;i++){rightChargePixels.setPixelColor(i, rightChargePixels.Color(0, 0, 0)); }//BLACK
+    }
+      //backward
+    if(chargeRight<500){
+      rightForwardPixels = (int)map(chargeRight, 500, 0, 0, NUMPIXELS);
+      for(int i=0;i<rightForwardPixels;i++){rightChargePixels.setPixelColor(i, rightChargePixels.Color(0,25,0));}//RED
+      for(int i=rightForwardPixels;i<NUMPIXELS;i++){rightChargePixels.setPixelColor(i, rightChargePixels.Color(0, 0, 0)); }//BLACK
+    }
+    rightChargePixels.show();
+
+// LEFT WHEEL READING
+//    averageReadingLeft = 500; //Guard for if LEFT WHEEL IS NOT ALR ATTACHED
+    chargeLeft = chargeLeft + map((int)averageReadingLeft, 0, 1023, -1200, 1200);
+    if(chargeLeft>550){chargeLeft = chargeLeft - map(chargeLeft, 500, 1023, 80, 350);}
+    if(chargeLeft<450){chargeLeft = chargeLeft + map(chargeLeft, 500, 0, 80, 350);}
+    if(chargeLeft>1023){chargeLeft = 1023;}//GUARD
+    if(chargeLeft<0){chargeLeft = 0;}
+
+    text[4]= map((int)chargeLeft, 0, 1023, -127, 127);//THE THING THAT IS SENT
+    //GUARD SO THE VALUE DOESN'T COME AS WEIRD
+    if(text[4]==0||text[4]==9||text[4]==10||text[4]==13||text[4]==32){
+      text[4] = 1;
+    }
+    if(text[4]==127){
+      text[4] = 126;
+    }
+//    Serial.println((int)text[4]);
+    //Left Charge Pixels Indicator
+      //forward
+    if(chargeLeft>500){
+      leftForwardPixels = (int)map(chargeLeft, 500, 1023, 0, NUMPIXELS);
+      for(int i=0;i<leftForwardPixels;i++){leftChargePixels.setPixelColor(i, leftChargePixels.Color(30,0,0));} //GREEN
+      for(int i=leftForwardPixels;i<NUMPIXELS;i++){leftChargePixels.setPixelColor(i, leftChargePixels.Color(0, 0, 0));} //BLACK
+    }
+      //backward
+    if(chargeLeft<500){
+      leftForwardPixels = (int)map(chargeLeft, 500, 0, 0, NUMPIXELS);
+      for(int i=0;i<leftForwardPixels;i++){leftChargePixels.setPixelColor(i, leftChargePixels.Color(0,25,0));}//RED
+      for(int i=leftForwardPixels;i<NUMPIXELS;i++){leftChargePixels.setPixelColor(i, pixels.Color(0, 0, 0));}//BLACK
+    }
+    leftChargePixels.show();
+    //
+    // Serial.print("Charge Left: ");
+    // Serial.println(chargeLeft);
+    Serial.print("Average Reading Left: ");
+    Serial.println(averageReadingLeft);
+    // Serial.print("things sent: ");Serial.print(text);
+    // Serial.print("text[3] in int: ");Serial.print((int)text[3]);
+    // Serial.print("text[4] in int: ");Serial.println((int)text[4]);
+    //Sending data
+    for(int i=0; i<9; i++){
+      mySerial.write(text[i]);
+    }
+//    Serial.println("Sending things");
+    //Formula for MP3 Volume & Neopixel
+    chargeMeter = chargeLeft + chargeRight;
+    if(chargeMeter<0){chargeMeter = 0;}
+    if(chargeMeter>2046){chargeMeter = 2046;}
+    mp3.setVolume((int8_t)map((int)chargeMeter, 0, 2046, 0, 15));
+//    Serial.print("averageReadingLeft: ");
+//    Serial.println((int)averageReadingLeft);
+    for(int i=0;i<(int)map(chargeMeter, 0, 1000, 0, NUMPIXELS);i++){
+      pixels.setPixelColor(i, pixels.Color(0,5,0)); // GREEN
+      pixels.show();
+    }
+    for(int i=(int)map(chargeMeter, 0, 1000, 0, NUMPIXELS);i<NUMPIXELS;i++){
+      pixels.setPixelColor(i, pixels.Color(0,0,0)); // BLACK
+      pixels.show();
+    }
+//    Serial.println("chargeMeter: ");
+//    Serial.println(chargeMeter);
+    //Cleanup for the next averaging
+    iteration = 1;
+    currentTime=millis();
+//    Serial.println(text);
+    averageReadingRight=0;
+    averageReadingLeft=0;
+    averageReadingGrabber=0;
+  }
+  //Averaging things
+  averageReadingRight = (averageReadingRight*(iteration-1)/iteration)+(double)((analogRead(RIGHT_SIDE_PIN))/iteration);
+  averageReadingLeft = (averageReadingLeft*(iteration-1)/iteration)+(double)((analogRead(LEFT_SIDE_PIN))/iteration);
+  iteration = iteration+1;
+}
